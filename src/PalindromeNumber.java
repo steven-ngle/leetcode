0df407e3e45b29dee1e9800037eff55e249c3301
stevennguyen <stevnguyen@protonmail.com>
2023-03-30 19:59:48 +0200

    commit

--- src/PalindromeNumber.java
+++ src/PalindromeNumber.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class PalindromeNumber {
+public class PalindromeNumber {
+    int reversedInt = 0;
+    boolean b = true;
+    public static void main(String[] args) {
+        // :)
+    }
+    public boolean isPalindrome(int x) {
+
+        if (x < 0) {
+            b = false;
+        } else {
+            reverseInt(x);
+        }
+        if (x == reversedInt) {
+            return b;
+        } else {
+            return b = false;
+        }
+    }
+    public int reverseInt(int i) {
+
+        while (i != 0) {
+            int rest = i % 10;
+            i = i / 10;
+            reversedInt = reversedInt * 10 + rest;
+        }
+        return reversedInt;
+    }
 }
