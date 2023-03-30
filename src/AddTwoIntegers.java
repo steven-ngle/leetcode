0637707b3db6949169a85fa8229fe74cb7535d82
stevennguyen <stevnguyen@protonmail.com>
2023-03-30 20:13:05 +0200

    commit

---
+++ src/AddTwoIntegers.java
@@ -0,0 +1,10 @@
+public class AddTwoIntegers {
+
+    public static void main(String[] args) {
+        // :)
+    }
+    public int sum(int num1, int num2) {
+        int i = num1 + num2;
+        return i;
+    }
+}
---
+++ src/RunningSumOf1dArray.java
@@ -0,0 +1,14 @@
+public class RunningSumOf1dArray {
+
+    public static void main(String[] args) {
+        // :)
+    }
+    public int[] runningSum(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] + sum;
+            sum = nums[i];
+        }
+        return nums;
+    }
+}
