e095ffe9233198ccac79aa81883d76b67e203c5e
stevennguyen <stevnguyen@protonmail.com>
2023-03-31 23:40:44 +0200

    commit

--- src/PlusOne.java
+++ src/PlusOne.java
@@ -10,8 +10,9 @@ public class PlusOne {
             if(digits[i]<9){
                 digits[i]++;
                 return digits;
+            } else {
+                digits[i] = 0;
             }
-            digits[i] = 0;
         }
 
         digits = new int[digits.length + 1];
