05bb743bcc1df67505f873fb019e69cd94ee67e2
steven-ngle <117310378+steven-ngle@users.noreply.github.com>
2023-06-03 12:16:49 +0200

    commit

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -2,12 +2,24 @@ public class TwoSum {
 
     public static void main(String[] args) {
         // :)
-
     }
-
     public int[] twoSum(int[] nums, int target) {
 
-        return nums;
+        int sum = 0;
+        int[] arr = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int k = i + 1; k < nums.length; k++) {
+                sum = nums[i] + nums[k];
+                if (sum == target) {
+                    arr[0] = i;
+                    arr[1] = k;
+                    return arr;
+                }
+            }
+        }
+        return arr;
     }
 
 }
+
