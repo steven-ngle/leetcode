7e93012f8122f67dffc9b63f037970749dfcfae1
steven-ngle <117310378+steven-ngle@users.noreply.github.com>
2023-07-16 14:54:12 +0200

    commit

--- src/RunningSumOf1dArray.java
+++ src/RunningSumOf1dArray.java
@@ -1,7 +1,9 @@
 public class RunningSumOf1dArray {
     // EASY
     public int[] runningSum(int[] nums) {
+
         int sum = 0;
+
         for (int i = 0; i < nums.length; i++) {
             nums[i] = nums[i] + sum;
             sum = nums[i];
