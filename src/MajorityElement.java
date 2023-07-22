4edd1b5a03353bb05bbb7d4d4a3f661d4b049d34
Steven Nguyen <stengu51202@gmail.com>
2023-07-22 22:34:58 +0200

    MajorityElement

---
+++ src/MajorityElement.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+public class MajorityElement {
+    // EASY
+    public int majorityElement(int[] nums) {
+
+        Arrays.sort(nums);
+
+        return nums[nums.length/2];
+    }
+}
