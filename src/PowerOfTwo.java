f6cb7c9333de24a00aac77394b70ae8a833dace6
Steven Nguyen <stengu51202@gmail.com>
2023-07-22 21:34:21 +0200

    PowerOfTwo

---
+++ src/PowerOfTwo.java
@@ -0,0 +1,19 @@
+public class PowerOfTwo {
+    // EASY
+    public boolean isPowerOfTwo(int n) {
+
+        boolean b = false;
+
+        if (n == 1) {
+            return true;
+        } else {
+            for (int i = 1; i < 1000000000; i = i*2) {
+                if (2 * i == n) {
+                    b = true;
+                    return b;
+                }
+            }
+        }
+        return b;
+    }
+}
