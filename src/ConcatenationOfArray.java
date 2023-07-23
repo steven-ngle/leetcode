7e39c26b8eb4655ead0b3d86ef18470d7efdd5c3
Steven Nguyen <stengu51202@gmail.com>
2023-07-23 18:54:43 +0200

    ConcatenationOfArray

---
+++ src/ConcatenationOfArray.java
@@ -0,0 +1,16 @@
+public class ConcatenationOfArray {
+    // EASY
+    public int[] getConcatenation(int[] nums) {
+
+        int[] arr = new int[2 * nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            arr[i] = nums[i];
+        }
+
+        for (int i = nums.length; i < arr.length; i++) {
+            arr[i] = nums[i - nums.length];
+        }
+        return arr;
+    }
+}
