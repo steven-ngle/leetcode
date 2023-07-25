68b4357c0b8ef18b3d97a8a78c4883c6463e819f
Steven Nguyen <stengu51202@gmail.com>
2023-07-25 20:05:54 +0200

    ConvertTheTemperature

--- src/ConvertTheTemperature.java
+++ src/ConvertTheTemperature.java
@@ -2,10 +2,10 @@ public class ConvertTheTemperature {
     // EASY
     public double[] convertTemperature(double celsius) {
 
-        double[] arr = new double[2];
+        double kelvin = celsius + 273.15;
+        double fahrenheit = celsius * 1.8 + 32;
 
-        arr[0] = celsius + 273.15;
-        arr[1] = celsius * 1.8 + 32;
+        double[] arr = {kelvin, fahrenheit};
 
         return arr;
     }
