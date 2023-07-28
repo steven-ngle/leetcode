762143a91f0b128b51ec6a1db8974103836e636b
Steven Nguyen <stengu51202@gmail.com>
2023-07-28 15:58:10 +0200

    commit

---
+++ src/SubtractTheProductAndSumOfDigitsOfAnInteger.java
@@ -0,0 +1,18 @@
+public class SubtractTheProductAndSumOfDigitsOfAnInteger {
+    // EASY
+    public int subtractProductAndSum(int n) {
+
+        int prod  = 1;
+        int sum = 0;
+
+        while (n > 0) {
+            int i = n % 10;
+            n = n / 10;
+            prod *= i;
+            sum += i;
+        }
+        int res = prod - sum;
+
+        return res;
+    }
+}
