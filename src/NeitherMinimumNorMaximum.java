8a2bdaabc414dcfd3caf7ea15d253d051e6f83fb
Steven Nguyen <stengu51202@gmail.com>
2023-07-29 00:21:04 +0200

    NeitherMinimumNorMaximum

---
+++ src/NeitherMinimumNorMaximum.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class NeitherMinimumNorMaximum {
+    // EASY
+    public int findNonMinOrMax(int[] nums) {
+
+        int m = nums.length;
+        int n = m / 2;
+
+        Arrays.sort(nums);
+
+        if (nums.length <= 2) {
+            return -1;
+        } else {
+            return nums[n];
+        }
+    }
+}
