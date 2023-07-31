13c254acbc7375112c2e87fb840c11bfc5418408
Steven Nguyen <stengu51202@gmail.com>
2023-07-31 18:57:33 +0200

    BestTimeToBuyAndSellStock

---
+++ src/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,17 @@
+public class BestTimeToBuyAndSellStock {
+    // EASY
+    public int maxProfit(int[] prices) {
+
+        int min = 10000;
+        int profit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (min > prices[i]) {
+                min = prices[i];
+            } else if (prices[i] - min > profit) {
+                profit = prices[i] - min;
+            }
+        }
+        return profit;
+    }
+}
