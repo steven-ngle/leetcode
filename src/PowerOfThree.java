e418e29760cdca36036ef709ccb57137d41c5070
Steven Nguyen <stengu51202@gmail.com>
2023-07-31 18:58:33 +0200

    PowerOfThree

---
+++ src/PowerOfThree.java
@@ -0,0 +1,16 @@
+public class PowerOfThree {
+    // EASY
+    public boolean isPowerOfThree(int n) {
+
+        if (n == 0) {
+            return false;
+        }
+
+        for (int i = 0; i < 20; i++) {
+            if (Math.pow(3, i) == n) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
