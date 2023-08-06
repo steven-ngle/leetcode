8c3648717c6bde815b1bdb943775b4e9dd56030e
steven-ngle <117310378+steven-ngle@users.noreply.github.com>
2023-08-06 15:21:22 +0200

    NumberOfEmployeesWhoMetTheTarget

---
+++ src/NumberOfEmployeesWhoMetTheTarget.java
@@ -0,0 +1,14 @@
+public class NumberOfEmployeesWhoMetTheTarget {
+    // EASY
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+
+        int counter = 0;
+
+        for (int i = 0; i < hours.length; i++) {
+            if (hours[i] >= target) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
