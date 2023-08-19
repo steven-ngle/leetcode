c86a77b8248e03e9b433ec8ad925d4fb0875d6c8
steven-ngle <117310378+steven-ngle@users.noreply.github.com>
2023-08-19 09:49:18 +0200

    KidsWithTheGreatestNumberOfCandies

---
+++ src/KidsWithTheGreatestNumberOfCandies.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class KidsWithTheGreatestNumberOfCandies {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+
+        List<Boolean>numberOfCandies = new ArrayList<>();
+        int max = 0;
+
+        for (int i = 0; i < candies.length; i++) {
+            if (max < candies[i]) {
+                max = candies[i];
+            }
+        }
+
+        for (int j = 0; j < candies.length; j++) {
+            if (candies[j] + extraCandies >= max) {
+                numberOfCandies.add(true);
+            } else {
+                numberOfCandies.add(false);
+            }
+        }
+        return numberOfCandies;
+    }
+}
