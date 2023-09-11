264bde10aa0dc686866df88414d68f5360d10fe9
Steven Nguyen <stengu51202@gmail.com>
2023-09-11 15:27:51 +0200

    RomanToInteger

---
+++ src/easy/RomanToInteger.java
@@ -0,0 +1,33 @@
+package easy;
+
+import java.util.HashMap;
+
+public class RomanToInteger {
+
+    public int romanToInt(String s) {
+
+        int result = 0;
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        for (int i = 1; i < s.length(); i++) {
+            if (map.get(s.charAt(i - 1)) < map.get(s.charAt(i))) {
+                result -= map.get(s.charAt(i - 1));
+            } else {
+                result += map.get(s.charAt(i - 1));
+            }
+        }
+
+        result += map.get(s.charAt(s.length() - 1));
+
+        return result;
+    }
+}
