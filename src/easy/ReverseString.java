c6556c5c9fae3a247f49e6716de0aa0a9d270af9
steven-ngle <117310378+steven-ngle@users.noreply.github.com>
2023-09-19 18:01:12 +0200

    ReverseString

---
+++ src/easy/ReverseString.java
@@ -0,0 +1,13 @@
+package easy;
+
+public class ReverseString {
+
+    public void reverseString(char[] s) {
+
+        for (int i = 0, j = s.length - 1; i < s.length/2; i++, j--) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+        }
+    }
+}
