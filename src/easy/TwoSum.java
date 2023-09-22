e390cd341c53a0d48157dd9f3984ca24cd7b9152
Steven Nguyen <stengu51202@gmail.com>
2023-09-22 18:41:08 +0200

    Updated TwoSum

--- src/easy/TwoSum.java
+++ src/easy/TwoSum.java
@@ -4,21 +4,17 @@ public class TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
 
-        int sum = 0;
-        int[] arr = new int[2];
+        int[] result = new int[2];
 
-        for (int i = 0; i < nums.length; i++) {
-            for (int n = i + 1; n < nums.length; n++) {
-                sum = nums[i] + nums[n];
-                if (sum == target) {
-                    arr[0] = i;
-                    arr[1] = n;
-                    return arr;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
                 }
             }
         }
-        return arr;
+        return result;
     }
-
 }
 
