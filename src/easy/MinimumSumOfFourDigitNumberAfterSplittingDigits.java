a18a4eef03f2edce1f36a12bae73be5290cb5e94
steven-ngle <117310378+steven-ngle@users.noreply.github.com>
2023-10-15 00:47:29 +0200

    tbd

---
+++ src/easy/MinimumSumOfFourDigitNumberAfterSplittingDigits.java
@@ -0,0 +1,13 @@
+package easy;
+
+public class MinimumSumOfFourDigitNumberAfterSplittingDigits {
+
+    // tbd
+    public int minimumSum(int num) {
+
+        int minSum = 0;
+
+        return minSum;
+    }
+}
+
