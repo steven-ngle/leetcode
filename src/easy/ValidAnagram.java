1e1f923ef00fb11cbf54f7e8811d4c02bc719cfa
steven-ngle <117310378+steven-ngle@users.noreply.github.com>
2023-10-21 19:48:27 +0200

    ValidAnagram

---
+++ src/easy/ValidAnagram.java
@@ -0,0 +1,28 @@
+package easy;
+
+import java.util.HashMap;
+
+public class ValidAnagram {
+
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> hm = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            hm.put(c, hm.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : t.toCharArray()) {
+            if (!hm.containsKey(c) || hm.get(c) == 0) {
+                return false;
+            }
+            hm.put(c,hm.get(c) - 1);
+        }
+
+        return true;
+    }
+}
