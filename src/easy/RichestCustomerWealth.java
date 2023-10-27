6c6caf28776f290bbe5d47d1833a2834b9ae631e
steven-ngle <117310378+steven-ngle@users.noreply.github.com>
2023-10-27 17:41:14 +0200

    RichestCustomerWealth

---
+++ src/easy/RichestCustomerWealth.java
@@ -0,0 +1,23 @@
+package easy;
+
+public class RichestCustomerWealth {
+
+    public int maximumWealth(int[][] accounts) {
+
+        int sum = 0;
+        int max = 0;
+
+        for (int i = 0; i < accounts.length; i++) {
+            for (int j = 0; j < accounts[i].length; j++) {
+                sum += accounts[i][j];
+                if (max < sum) {
+                    max = sum;
+
+                }
+            }
+            sum = 0;
+        }
+
+        return max;
+    }
+}
