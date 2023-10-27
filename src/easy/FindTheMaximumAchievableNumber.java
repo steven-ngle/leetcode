0bab52c1afd52bf02570026fd59b0dac54b82587
steven-ngle <117310378+steven-ngle@users.noreply.github.com>
2023-10-27 17:13:48 +0200

    FindTheMaximumAchievableNumber

---
+++ src/easy/FindTheMaximumAchievableNumber.java
@@ -0,0 +1,11 @@
+package easy;
+
+public class FindTheMaximumAchievableNumber {
+
+    public int theMaximumAchievableX(int num, int t) {
+
+        int x = num + 2 * t;
+
+        return x;
+    }
+}
