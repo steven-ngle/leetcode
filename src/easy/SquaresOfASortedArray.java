0655d4aa4272cabc120c20bf045f3380a97d7a78
steven-ngle <117310378+steven-ngle@users.noreply.github.com>
2023-10-31 13:12:24 +0100

    SquaresOfASortedArray

---
+++ src/easy/SquaresOfASortedArray.java
@@ -0,0 +1,17 @@
+package easy;
+
+import java.util.Arrays;
+
+public class SquaresOfASortedArray {
+
+    public int[] sortedSquares(int[] nums) {
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] *= nums[i];
+        }
+
+        Arrays.sort(nums);
+
+        return nums;
+    }
+}
