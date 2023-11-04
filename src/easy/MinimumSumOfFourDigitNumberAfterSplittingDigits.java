42667ecfe3cff1f2a5d042d2cb6dde157d4027a6
steven-ngle <117310378+steven-ngle@users.noreply.github.com>
2023-11-04 23:08:38 +0100

    MinimumSumOfFourDigitNumberAfterSplittingDigits

--- src/easy/MinimumSumOfFourDigitNumberAfterSplittingDigits.java
+++ src/easy/MinimumSumOfFourDigitNumberAfterSplittingDigits.java
@@ -1,13 +1,25 @@
 package easy;
 
+import java.util.Arrays;
+
 public class MinimumSumOfFourDigitNumberAfterSplittingDigits {
 
-    // tbd
     public int minimumSum(int num) {
 
-        int minSum = 0;
+        int[] arr = new int[2];
+        int sum = 0;
 
-        return minSum;
+        for (int i = 0; i < 4; i++) {
+            sum = num % 10;
+            arr[i] = sum;
+            num /= 10;
+        }
+
+        Arrays.sort(arr);
+        int new1 = arr[0] * 10 + arr[3];
+        int new2 = arr[1] * 10 + arr[2];
+
+        return new1 + new2;
     }
 }
 
