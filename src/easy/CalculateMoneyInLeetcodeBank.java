7220a27cedcca8ab32921b6cb7f0883474a231b5
steven-ngle <117310378+steven-ngle@users.noreply.github.com>
2023-12-06 17:40:13 +0100

    CalculateMoneyInLeetcodeBank

---
+++ src/easy/CalculateMoneyInLeetcodeBank.java
@@ -0,0 +1,27 @@
+package easy;
+
+public class CalculateMoneyInLeetcodeBank {
+
+    public int totalMoney(int n) {
+
+        int sum = 0;
+        int dollar = 1;
+        int monday = 1;
+        int counter = 0;
+
+        while (n > 0) {
+            sum += dollar;
+            dollar++;
+            counter++;
+            if (counter == 7) {
+                monday++;
+                dollar = monday;
+                counter = 0;
+            }
+            n--;
+        }
+
+
+        return sum;
+    }
+}
