c07e995a6cc0f4a04c78ebfbf49f9f71b8703f87
steven-ngle <117310378+steven-ngle@users.noreply.github.com>
2023-12-06 17:56:48 +0100

    StrongPasswordCheckerII

--- src/easy/StrongPasswordCheckerII.java
+++ src/easy/StrongPasswordCheckerII.java
@@ -4,8 +4,40 @@ public class StrongPasswordCheckerII {
 
     public boolean strongPasswordCheckerII(String password) {
 
-       //tbd
+        if (password.length() < 8) {
+            return false;
+        }
 
-        return false;
+        String specialChar = "!@#$%^&*()-+";
+        boolean hasLowercase = false;
+        boolean hasUppercase = false;
+        boolean hasDigit = false;
+        boolean hasSpecialChar = false;
+        boolean hasAdjacent = false;
+        char prevChar = '\0';
+
+        for (int i = 0; i < password.length(); i++) {
+            char c = password.charAt(i);
+            if (Character.isLowerCase(c)) {
+                hasLowercase = true;
+            } else if (Character.isUpperCase(c)) {
+                hasUppercase = true;
+            } else if (Character.isDigit(c)) {
+                hasDigit = true;
+            } else if (specialChar.contains(String.valueOf(c))){
+                hasSpecialChar = true;
+            }
+
+            if (c == prevChar) {
+                hasAdjacent = true;
+            }
+            prevChar = c;
+        }
+
+        if (!hasLowercase || !hasUppercase || !hasDigit || !hasSpecialChar || hasAdjacent) {
+            return false;
+        }
+
+        return true;
     }
 }
