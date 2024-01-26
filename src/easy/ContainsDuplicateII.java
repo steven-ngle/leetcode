d4d90b7a5cc85e3c5a691a5d63d68603bdb9b776
Steven Nguyen <117310378+steven-ngle@users.noreply.github.com>
2024-01-26 10:04:27 +0100

    ContainsDuplicateII

---
+++ src/easy/ContainsDuplicateII.java
@@ -0,0 +1,24 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author : Steven
+ * @created : 26.01.2024, Freitag
+ **/
+public class ContainsDuplicateII {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) {
+                return true;
+            }
+            map.put(nums[i], i);
+        }
+
+        return false;
+    }
+}
