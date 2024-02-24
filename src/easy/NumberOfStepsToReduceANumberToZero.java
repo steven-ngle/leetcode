cc9499d2dd1701d7de2f4cff7679844609c8a7d5
Steven_Nguyen <stengu51202@gmail.com>
2024-02-24 14:41:41 +0100

    NumberOfStepsToReduceANumberToZero

---
+++ src/easy/NumberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,17 @@
+package easy;
+
+public class NumberOfStepsToReduceANumberToZero {
+
+    public int numberOfSteps(int num) {
+
+        int count = 0;
+
+        while (num > 0) {
+            if (num % 2 != 0) num--;
+            else num /= 2;
+            count++;
+        }
+
+        return count;
+    }
+}
