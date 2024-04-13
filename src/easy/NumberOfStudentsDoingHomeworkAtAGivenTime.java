a2a9193af9e0073c9f984764cc0228d2355911a9
Steven Nguyen <stengu51202@gmail.com>
2024-04-13 11:03:41 +0200

    NumberOfStudentsDoingHomeworkAtAGivenTime

---
+++ src/easy/NumberOfStudentsDoingHomeworkAtAGivenTime.java
@@ -0,0 +1,15 @@
+package easy;
+
+public class NumberOfStudentsDoingHomeworkAtAGivenTime {
+
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+
+        int count = 0;
+
+        for (int i = 0; i < startTime.length; i++) {
+            if (queryTime >= startTime[i] && queryTime <= endTime[i]) count++;
+        }
+
+        return count;
+    }
+}
