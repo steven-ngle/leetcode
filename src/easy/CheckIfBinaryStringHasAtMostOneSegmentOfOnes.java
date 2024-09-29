fde0bc4aaa2b54630bf4fb4d7071199046d26e91
Steven Nguyen <stengu51202@gmail.com>
2024-09-29 17:44:32 -0400

    CheckIfBinaryStringHasAtMostOneSegmentOfOnes

---
+++ src/easy/CheckIfBinaryStringHasAtMostOneSegmentOfOnes.java
@@ -0,0 +1,13 @@
+package easy;
+
+public class CheckIfBinaryStringHasAtMostOneSegmentOfOnes {
+
+    public boolean checkOnesSegment(String s) {
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == '0' && s.charAt(i + 1) == '1') return false;
+        }
+
+        return true;
+    }
+}
